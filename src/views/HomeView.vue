dafc2de6b919a66c13090e7f831b3f7d1fe50417
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-20 21:16:25 +0700

    delete home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue'
+import Hero from '@/components/Hero.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Hero />
 </template>
