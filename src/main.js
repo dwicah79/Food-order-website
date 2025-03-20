5b34664af20d3bb221cbd59b8fd4f514ca0d9d0b
dwicah79 <dwi2100018405@webmail.uad.ac.id>
2025-03-20 21:15:24 +0700

    add font awesome library

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCartShopping, faHome, faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faCartShopping, faHome, faUtensils, faCartShopping, faWhatsapp)
 
 const app = createApp(App)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
